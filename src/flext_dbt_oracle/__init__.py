c86950e1cffd094c45ea13c13183221b10f3d322
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-07-06 02:46:40 -0300

    chore(codegen): inline lazy __init__.py, eliminate _exports.py sidecar
    Regenerated by 'flext_infra codegen init --apply': PEP 562 lazy imports are
    now inlined directly into each package __init__.py; the separate _exports.py
    lazy-map sidecar is removed (superseded by the inline map).

--- src/flext_dbt_oracle/__init__.py
+++ src/flext_dbt_oracle/__init__.py
@@ -5,7 +5,7 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from flext_core.lazy import install_lazy_exports
+from flext_core.lazy import build_lazy_import_map, install_lazy_exports
 from flext_dbt_oracle.__version__ import (
     __author__,
     __author_email__,
@@ -16,82 +16,53 @@ from flext_dbt_oracle.__version__ import (
     __version__,
     __version_info__,
 )
-from flext_dbt_oracle._exports import FLEXT_DBT_ORACLE_LAZY_IMPORTS
 
 if TYPE_CHECKING:
-    from flext_db_oracle import d as d, e as e, h as h, r as r, x as x
-    from flext_dbt_oracle.base import (
-        FlextDbtOracleServiceBase as FlextDbtOracleServiceBase,
-        s as s,
-    )
-    from flext_dbt_oracle.constants import (
-        FlextDbtOracleConstants as FlextDbtOracleConstants,
-        c as c,
-    )
-    from flext_dbt_oracle.models import (
-        FlextDbtOracleModels as FlextDbtOracleModels,
-        m as m,
-    )
-    from flext_dbt_oracle.protocols import (
-        FlextDbtOracleProtocols as FlextDbtOracleProtocols,
-        p as p,
-    )
-    from flext_dbt_oracle.settings import (
-        FlextDbtOracleSettings as FlextDbtOracleSettings,
-    )
-    from flext_dbt_oracle.typings import (
-        FlextDbtOracleTypes as FlextDbtOracleTypes,
-        t as t,
-    )
-    from flext_dbt_oracle.utilities import (
-        FlextDbtOracleUtilities as FlextDbtOracleUtilities,
-        u as u,
-    )
-
-
-_LAZY_IMPORTS = FLEXT_DBT_ORACLE_LAZY_IMPORTS
-
-
-_EAGER_EXPORTS = (
-    __author__,
-    __author_email__,
-    __description__,
-    __license__,
-    __title__,
-    __url__,
-    __version__,
-    __version_info__,
+    from flext_db_oracle import d, e, h, r, x
+    from flext_dbt_oracle.base import FlextDbtOracleServiceBase, s
+    from flext_dbt_oracle.constants import FlextDbtOracleConstants, c
+    from flext_dbt_oracle.models import FlextDbtOracleModels, m
+    from flext_dbt_oracle.protocols import FlextDbtOracleProtocols, p
+    from flext_dbt_oracle.settings import FlextDbtOracleSettings
+    from flext_dbt_oracle.typings import FlextDbtOracleTypes, t
+    from flext_dbt_oracle.utilities import FlextDbtOracleUtilities, u
+_LAZY_IMPORTS = build_lazy_import_map(
+    {
+        ".base": (
+            "FlextDbtOracleServiceBase",
+            "s",
+        ),
+        ".constants": (
+            "FlextDbtOracleConstants",
+            "c",
+        ),
+        ".models": (
+            "FlextDbtOracleModels",
+            "m",
+        ),
+        ".protocols": (
+            "FlextDbtOracleProtocols",
+            "p",
+        ),
+        ".settings": ("FlextDbtOracleSettings",),
+        ".typings": (
+            "FlextDbtOracleTypes",
+            "t",
+        ),
+        ".utilities": (
+            "FlextDbtOracleUtilities",
+            "u",
+        ),
+        "flext_db_oracle": (
+            "d",
+            "e",
+            "h",
+            "r",
+            "x",
+        ),
+    },
 )
 
-
-_PUBLIC_EXPORTS: tuple[str, ...] = (
-    "FlextDbtOracleConstants",
-    "FlextDbtOracleModels",
-    "FlextDbtOracleProtocols",
-    "FlextDbtOracleServiceBase",
-    "FlextDbtOracleSettings",
-    "FlextDbtOracleTypes",
-    "FlextDbtOracleUtilities",
-    "__author__",
-    "__author_email__",
-    "__description__",
-    "__license__",
-    "__title__",
-    "__url__",
-    "__version__",
-    "__version_info__",
-    "c",
-    "d",
-    "e",
-    "h",
-    "m",
-    "p",
-    "r",
-    "s",
-    "t",
-    "u",
-    "x",
-)
 
 __all__: tuple[str, ...] = (
     "FlextDbtOracleConstants",
@@ -127,5 +98,5 @@ install_lazy_exports(
     __name__,
     globals(),
     _LAZY_IMPORTS,
-    public_exports=_PUBLIC_EXPORTS,
+    public_exports=__all__,
 )
