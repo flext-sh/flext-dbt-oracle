7903bb7e369f646aeb6de071c5db1a17ebcfebfa
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-09-21 14:40:13 -0300

    chore(gen): converge projections at the integration tip (fleet fixed point)

--- src/flext_dbt_oracle/__init__.py
+++ src/flext_dbt_oracle/__init__.py
@@ -20,7 +20,8 @@ from .__version__ import (
 )
 
 if TYPE_CHECKING:
-    from flext_core import d, e, h, r, x
+    from flext_core import d, h, r, x
+    from flext_db_oracle import e
 
     from . import services
     from .__version__ import FlextDbtOracleVersion
@@ -92,7 +93,8 @@ _LAZY_IMPORTS = MappingProxyType(
             ".services.client": ("FlextDbtOracleServicesClient",),
             ".typings": ("FlextDbtOracleTypes", "t"),
             ".utilities": ("FlextDbtOracleUtilities", "u"),
-            "flext_core": ("d", "e", "h", "r", "x"),
+            "flext_core": ("d", "h", "r", "x"),
+            "flext_db_oracle": ("e",),
         }),
         alias_groups=MappingProxyType({}),
         sort_keys=False,
