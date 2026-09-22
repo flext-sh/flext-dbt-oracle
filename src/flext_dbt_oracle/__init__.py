f606c3eb50593661a07c4622d589cd14d0e540a7
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-09-22 18:10:29 -0300

    chore(gen): adopt the conformance fixed-point render

--- src/flext_dbt_oracle/__init__.py
+++ src/flext_dbt_oracle/__init__.py
@@ -21,26 +21,23 @@ from .__version__ import (
 
 if TYPE_CHECKING:
     from flext_cli import cli
-    from flext_db_oracle import db_oracle, e
     from flext_meltano import meltano
-    from pydantic_core import from_json, to_json, to_jsonable_python
 
-    from flext_core import core, d, h, lazy_attribute, r, x
+    from flext_core import core, d, e, h, lazy_attribute, r, x
 
     from . import services
-    from .__version__ import FlextDbtOracleVersion
     from ._config import FlextDbtOracleConfig, config
     from ._settings import FlextDbtOracleSettings, settings
     from .api import FlextDbtOracle
     from .base import FlextDbtOracleServiceBase, FlextDbtOracleServiceBase as s
     from .cli import FlextDbtOracleCliService, main
-    from .constants import FlextDbtOracleConstants, FlextDbtOracleConstants as c
-    from .models import FlextDbtOracleModels, FlextDbtOracleModels as m
-    from .protocols import FlextDbtOracleProtocols, FlextDbtOracleProtocols as p
+    from .constants import FlextDbtOracleConstants, c
+    from .models import FlextDbtOracleModels, m
+    from .protocols import FlextDbtOracleProtocols, p
     from .services.base import FlextDbtOracleServicesBase
     from .services.client import FlextDbtOracleServicesClient
-    from .typings import FlextDbtOracleTypes, FlextDbtOracleTypes as t
-    from .utilities import FlextDbtOracleUtilities, FlextDbtOracleUtilities as u
+    from .typings import FlextDbtOracleTypes, t
+    from .utilities import FlextDbtOracleUtilities, u
 __all__: tuple[str, ...] = (
     "FlextDbtOracle",
     "FlextDbtOracleCliService",
@@ -54,7 +51,6 @@ __all__: tuple[str, ...] = (
     "FlextDbtOracleSettings",
     "FlextDbtOracleTypes",
     "FlextDbtOracleUtilities",
-    "FlextDbtOracleVersion",
     "__author__",
     "__author_email__",
     "__description__",
@@ -64,12 +60,16 @@ __all__: tuple[str, ...] = (
     "__version__",
     "__version_info__",
     "c",
+    "cli",
     "config",
+    "core",
     "d",
     "e",
     "h",
+    "lazy_attribute",
     "m",
     "main",
+    "meltano",
     "p",
     "r",
     "s",
@@ -83,7 +83,6 @@ __all__: tuple[str, ...] = (
 _LAZY_IMPORTS = MappingProxyType(
     build_lazy_import_map(
         MappingProxyType({
-            ".__version__": ("FlextDbtOracleVersion",),
             "._config": ("FlextDbtOracleConfig", "config"),
             "._settings": ("FlextDbtOracleSettings", "settings"),
             ".api": ("FlextDbtOracle",),
@@ -98,10 +97,8 @@ _LAZY_IMPORTS = MappingProxyType(
             ".typings": ("FlextDbtOracleTypes", "t"),
             ".utilities": ("FlextDbtOracleUtilities", "u"),
             "flext_cli": ("cli",),
-            "flext_core": ("core", "d", "h", "lazy_attribute", "r", "x"),
-            "flext_db_oracle": ("db_oracle", "e"),
+            "flext_core": ("core", "d", "e", "h", "lazy_attribute", "r", "x"),
             "flext_meltano": ("meltano",),
-            "pydantic_core": ("from_json", "to_json", "to_jsonable_python"),
         }),
         alias_groups=MappingProxyType({}),
         sort_keys=False,
